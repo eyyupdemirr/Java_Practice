5b27481369dbd6d1d0350f6acc5de91d5dbb069e
eyyup demir <ludwigsburg100@gmail.com>
2023-09-06 16:29:00 +0200

    6,9,23

---
+++ src/reCAP/IfteSusluParentezinKapsami.java
@@ -0,0 +1,24 @@
+package reCAP;
+
+public class IfteSusluParentezinKapsami {
+    public static void main(String[] args) {
+
+        int a = 120;
+        int b = 50;
+        int c = 100;
+
+        if (a < b)
+            System.out.println(" a b den kucuk");
+            System.out.println(" a b den kucuk");
+            System.out.println(" a b den kucuk");
+
+
+        System.out.println("***********************************************");
+
+        if (a> b) {
+            System.out.println(" a b den kucuk");
+            System.out.println(" a c den kucuk");
+            System.out.println(" a d den kucuk");
+        }
+    }
+}
